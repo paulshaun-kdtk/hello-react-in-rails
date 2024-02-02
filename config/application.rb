5e477a5c9f71d26258f8fe0acd87deddfd12a586
unknown <kudzpaul5@gmail.com>
2024-02-02 17:43:46 +0200

    l reviewers

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module HelloRails
     config.load_defaults 7.1
 
     config.assets.enabled = true
-    config.assets.precompile += %w( builds/application.js )
+    config.assets.precompile += %w[builds/application.js]
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
