223799a751119a82eee1d5453169c20978cc95c9
unknown <kudzpaul5@gmail.com>
2024-02-02 17:39:48 +0200

    trial

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module HelloRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.assets.enabled = true
+    config.assets.precompile += %w( builds/application.js )
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
